aa029d02b05e0727aec06b822c2c3e26d7e04d2f
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2022-06-17 16:45:57 +0900

    기본 세팅 추가했습니다 -성현-

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const port = 4000;
 const app = express();
+const Cors = require('cors');
+const profileRouter = require('./routes/profile.route');
+const userRouter = require('./routes/user.route');
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.use(Cors());
+app.use('/', [userRouter, profileRouter]);
 
 app.listen(port, () => {
   console.log('🟢', port, '번 포트');

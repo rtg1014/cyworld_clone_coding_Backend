43300561674f5331a323e53f81ffbca578fc66ee
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2022-06-23 04:39:18 +0900

    노드메일러를 활용한 비밀번호 분실시 초기화 기능 구현하였습니다

--- controllers/passwordFInd.controller.js
+++ controllers/passwordFInd.controller.js
@@ -1,8 +1,21 @@
+const { array } = require('joi');
 const nodeMailer = require('nodemailer');
 const senderInfo = require('../config/sender.json');
+const Bcrypt = require('bcrypt');
+const { User } = require('../models');
 
-async function mailSender() {
-  // // 김성현 메일 보내는 세팅 =====================================================
+// 비밀번호 분실시 이메일 발송 start -------------------------------------------------
+let authNumber = [];
+
+async function mailSender(req) {
+  //
+  const { email } = req.body;
+  let text = Math.floor(Math.random() * 100000);
+  authNumber.push(text);
+
+  console.log(authNumber);
+
+  //  메일 보내는 옵션 세팅 함수 =====================================================
   const mailPoster = nodeMailer.createTransport({
     service: 'naver',
     host: 'smtp.naver.com',
@@ -12,20 +25,18 @@ async function mailSender() {
       pass: senderInfo.pass,
     },
   });
-  console.log('fffff');
-  // // 김성현 메일 받는 세팅 ==========================================
+
+  // // 김성현 메일 받는 옵션 세팅 함수 ==========================================
 
   const mailOption = {
-    from: 'rtg1014@naver.com',
-    to: 'rtg1014@naver.com',
-    subject: '비밀번호 분실을 위한 메일입니다.',
-    text: '축하합니다 드디어 노드메일러를 성공하셨군요!!',
+    from: senderInfo.user,
+    to: email,
+    subject: '인증번호를 위한 메일입니다.',
+    text: ' 인증번호는 ' + `${text}`,
   };
-
-  console.log(3);
+  console.log(1111, mailOption);
 
   // // 메일 전송
-
   mailPoster.sendMail(mailOption, function (error, info) {
     if (error) {
       console.log('에러' + error);
@@ -35,11 +46,48 @@ async function mailSender() {
   });
 }
 
-console.log('끝');
-module.exports = { mailSender };
+// 비밀번호 분실시 이메일 발송 end ---------------------------------------------------
 
-// module.exports.sendPw = async (req, res) => {};
+// 이메일로 받은 인증번호 체크===========================================================
+async function authCheck(req, res) {
+  try {
+    const { emailAuthNumber } = req.body;
+    if (authNumber.includes(emailAuthNumber)) {
+      res.status(200).send({
+        Message: '인증번호가 일치합니다.',
+      });
+      console.log(authNumber);
+    } else {
+      res.status(400).send({
+        Message: '인증번호가 일치하지 않습니다.',
+      });
+    }
+  } catch (err) {
+    console.log(err),
+      res.status(401).send({
+        errorMessage: '인증방식이 잘못되었습니다.',
+      });
+  }
+}
 
-// module.exports.search.id;
+// 인증번호 확인 후 비밀번호 변경 =====================================================
+async function changePassword(req, res, next) {
+  const { email } = req.params;
+  const { password } = req.body;
+  try {
+    const salt = await Bcrypt.genSalt();
+    const pwhashed = await Bcrypt.hash(password, salt);
+    await User.update({ password: pwhashed }, { where: { email: email } });
+    res.status(200).send({
+      Message: '비밀번호가 변경됐습니다.',
+    });
+  } catch (err) {
+    console.error(err),
+      res.status(401).send({
+        Message: '문자형으로 입력해주세요',
+      });
+    next(error);
+  }
+}
 
-// module.exports.search.pw;
+module.exports = { mailSender, authCheck, changePassword };
--- routes/password.route.js
+++ routes/password.route.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const passwordFInd = require('../controllers/passwordFInd.controller');
+const passwordFind = require('../controllers/passwordFInd.controller');
 
 // 비밀번호 분실시 이메일 발송 test
-router.post('/send/pw', passwordFInd.mailSender);
+router.post('/send/pw', passwordFind.mailSender);
 
-// router.post('/search/id', passwordFInd.controller.search.id);
+//  이메일 인증번호 체크
+router.post('/send/authCheck', passwordFind.authCheck);
 
-// router.post('/search/pw', passwordFInd.controller.search.pw);
+// 비밀번호 변경
+router.patch('/send/changePassword/:email', passwordFind.changePassword);
+
+// api 인증번호를 받고 맞는지 아닌지 확인까지 하는 api 생성
+
+// 인증번호 저장할곳도 만들기/생각하기
+
+// 인증 확인 됫으면 그 값을 받아서 비밀번호를 변경하는 api 만들기
+
+// (req, res) => {
+//     const { email } = req.body;
+//     // 이메일이 있는지 검사 (가입한 이메일인지)
+
+//     let emailParam = {
+//       toEmail: email,
+//       subject: '비밀번호 분실을 위한 메일입니다.',
+//       text: '비밀번호 찾기의 인증번호는 랜덤숫자 입니다.',
+//     };
+
+//     mailer.mailSender(emailParam);
+
+//     res.status(200).send('인증번호 보내기 성공');
+//   });
 
 module.exports = router;

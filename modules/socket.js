8f27442ce1af74fdd78da542e1b4101b9ed9dfa0
Sungjun Kim <hzcd108@gmail.com>
2022-06-22 20:20:42 +0900

    라이브 채팅 수정

--- modules/socket.js
+++ modules/socket.js
@@ -6,6 +6,8 @@ const io = socketIo(http, { cors: { origin: '*' } });
 const checkCounts = (room) => io.sockets.adapter.rooms.get(room)?.size || 0;
 
 module.exports = io.on('connection', function (socket) {
+  let { name, room } = socket;
+
   socket.emit('search', {
     rooms: [
       ...new Set(
@@ -15,45 +17,43 @@ module.exports = io.on('connection', function (socket) {
   });
 
   socket.on('newUser', (data) => {
-    socket.name = data.name;
-    socket.room = data.room;
+    name = data.name;
+    room = data.room;
 
-    socket.join(socket.room);
+    socket.join(room);
+
     console.log([
       ...new Set(
         [...io.sockets.adapter.sids.values()].map((data) => [...data][1])
       ),
     ]);
-    io.to(socket.room).emit('update', {
+
+    io.to(room).emit('update', {
       type: 'connect',
-      name: socket.name,
-      message: '님이 접속하였습니다.',
-      count: checkCounts(socket.room),
+      name,
+      count: checkCounts(room),
     });
-    console.log(socket.name + ' 님이 접속하였습니다.');
-    console.log(checkCounts(socket.room) + '명 접속중');
+    console.log(`${name}님이 참가했습니다. (총 ${checkCounts(room)}명)`);
   });
 
   socket.on('message', (message) => {
-    io.to(socket.room).emit('update', {
-      name: socket.name,
+    io.to(room).emit('update', {
+      type: 'message',
+      name,
       message,
-      count: checkCounts(socket.room),
+      count: checkCounts(room),
     });
-    console.log(socket.name + ' : ' + message);
-    console.log(checkCounts(socket.room) + '명 접속중');
+    console.log(`${name} : ${message} (총 ${checkCounts(room)}명)`);
   });
 
   socket.on('disconnect', () => {
-    socket.leave(socket.room);
+    socket.leave(room);
 
-    io.to(socket.room).emit('update', {
+    io.to(room).emit('update', {
       type: 'disconnect',
-      name: socket.name,
-      message: '님이 나가셨습니다.',
-      count: checkCounts(socket.room),
+      name: name,
+      count: checkCounts(room),
     });
-    console.log(socket.name + '님이 나가셨습니다.');
-    console.log(checkCounts(socket.room) + '명 접속중');
+    console.log(`${name}님이 나갔습니다. (총 ${checkCounts(room)}명)`);
   });
 });
